97918c607caa877698860dae9ba9599ab73d7299
dschoenberg <dschoenberg@gmail.com>
2018-06-10 20:09:21 +0000

    added encoder class

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def handle(pin):
 	global last_state, counter
 	state1 = GPIO.input(btn1)
 	state2 = GPIO.input(btn2)
-	#print('handle' + str(state1) + ', ' + str(state2))
+	print('handle' + str(state1) + ', ' + str(state2))
 	if state1 != last_state:
 		if state1 != state2:
 			counter += 1
@@ -32,6 +32,41 @@ def handle(pin):
 
 GPIO.add_event_detect(btn1, GPIO.BOTH, handle)
 
+import urllib2
+import json
+
+playerList = None
+winnerIndex = 0
+loserIndex = 0
+
+def updateDisplay():
+	print('winner: ' + playerList[winnerIndex]["firstName"] + ' ' + playerList[winnerIndex]["lastName"])
+
+def updateWinner(forward):
+	global winnerIndex
+
+	if forward:
+		winnerIndex = winnerIndex + 1
+		if winnerIndex >= len(playerList):
+			winnerIndex = 0
+	else:
+		winnerIndex = winnerIndex - 1
+		if winnerIndex < 0:
+			winnerIndex = len(playerList) - 1
+	updateDisplay()
+
+def getPlayers():
+	global playerList
+	url = "http://hi-ping-pong.herokuapp.com/players"
+	req = urllib2.Request(url)
+	opener = urllib2.build_opener()
+	f = opener.open(req)
+	playerList = json.loads(f.read())
+	
+
 print('off we go')
+getPlayers()
+
 while True:
 	time.sleep(1)
+
