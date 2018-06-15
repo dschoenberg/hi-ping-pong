4337206d70de02c4cf099730387076520a2cb27d
dschoenberg <dschoenberg@gmail.com>
2018-06-15 01:50:32 +0000

    updated submit logic

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ import time
 import rotary_encoder
 import urllib2
 import json
+import requests
 
 GPIO.setmode(GPIO.BOARD)
 
 winnerPin1 = 10
 winnerPin2 = 12
-loserPin1 = 3
-loserPin2 = 5
+loserPin1 = 16
+loserPin2 = 18
 submitPin = 7
 
 playerList = None
@@ -19,7 +20,8 @@ winnerIndex = 0
 loserIndex = 0
 
 def updateDisplay():
-	print('winner: ' + playerList[winnerIndex]["firstName"] + ' ' + playerList[winnerIndex]["lastName"])
+	print('winner: ' + playerList[winnerIndex]["firstName"])
+	print('loser:  ' + playerList[loserIndex]["firstName"])
 
 def updateWinner(forward):
 	global winnerIndex
@@ -32,6 +34,9 @@ def updateWinner(forward):
 		winnerIndex = winnerIndex - 1
 		if winnerIndex < 0:
 			winnerIndex = len(playerList) - 1
+	if winnerIndex == loserIndex:
+		updateWinner(forward)
+
 	updateDisplay()
 
 def updateLoser(forward):
@@ -45,18 +50,38 @@ def updateLoser(forward):
 		loserIndex = loserIndex - 1
 		if loserIndex < 0:
 			loserIndex = len(playerList) - 1
+	if loserIndex == winnerIndex:
+		updateLoser(forward)
+	
 	updateDisplay()
 
-def submitGame():
+submitting = False
+def submitGame(pin):
+	global submitting
 	print('submitting game...')
+	print('winner: ' + playerList[winnerIndex]['_id'])
+	print('loser:  ' + playerList[loserIndex]['_id'])
+
+	data = {
+		'winner':playerList[winnerIndex]['_id'],
+		'loser':playerList[loserIndex]['_id']
+	}
+
+	if not submitting:
+		submitting = True #I regret nothing!
+		req = requests.post("http://hi-ping-pong.herokuapp.com/match", data=data)
+		print(req)
+		submitting = False
 
 def getPlayers():
 	global playerList
+	print('requesting players')
 	url = "http://hi-ping-pong.herokuapp.com/players"
 	req = urllib2.Request(url)
 	opener = urllib2.build_opener()
 	f = opener.open(req)
 	playerList = json.loads(f.read())
+	print(str(playerList))
 	print(str(len(playerList)) + ' players loaded')
 
 if __name__ == "__main__":
